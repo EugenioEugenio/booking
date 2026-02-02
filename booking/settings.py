b0d4040627680e9841e715a28794c591c5f1e41f
Eugenio <noescribaaqui@gmail.com>
2026-02-02 14:56:37 +0300

    final

--- booking/settings.py
+++ booking/settings.py
@@ -119,4 +119,20 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 
-AUTH_USER_MODEL = "api.ApiUser"
+#AUTH_USER_MODEL = "api.ApiUser"
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'api.ApiUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ]
+}
